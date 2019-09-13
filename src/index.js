f5312403b242871d988613aee07dcb12d5cf3645
waspryo <wasp2123321@gmail.com>
2019-09-13 14:17:51 +0700

    combine reducers

--- src/index.js
+++ src/index.js
@@ -3,39 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
 
-//STORE -> GLOBALIZED STATE
-
-//ACTION INCREMENT
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  };
-};
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-  };
-};
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return -1;
-  }
-};
-
-let store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
