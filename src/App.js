ce60de1f1a4e27e03e6d553df61b9be3128ef2e4
waspryo <wasp2123321@gmail.com>
2019-09-14 12:43:08 +0700

    add increment button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./actions";
+ 
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
+      {isLogged ? <h3>Valuable Information I should'nt see</h3> : ''}
     </div>
   );
 }
